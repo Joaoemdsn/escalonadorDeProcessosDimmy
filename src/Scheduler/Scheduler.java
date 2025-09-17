a6dfaa3a5444d7466ac13bb33220567c04aab84f
Lucasrs3 <lucasrsoares2k16@gmail.com>
2025-09-17 01:29:32 -0300

    feat: implementação de lógica de execução do ciclo da CPU e priorização de processos

--- src/Scheduler/Scheduler.java
+++ src/Scheduler/Scheduler.java
@@ -34,6 +34,47 @@ public class Scheduler {
     }
 
     public void executarCiclosDeCPU(){
+        System.out.println("\n--- INICIANDO NOVO CICLO DE CPU ---");
+
+        Processo processoExecutando = null;
         
-    }
+        if (!lista_bloqueados.estaVazia()) {
+            Processo pDesbloqueado = lista_bloqueados.removerInicio();
+            System.out.println("EVENTO: Processo " + pDesbloqueado.getNome() + " desbloqueado e retornado a sua lista de prioridade original.");
+            switch (pDesbloqueado.getPrioridade()) {
+                case 1:
+                    lista_alta_prioridade.adicionarFim(pDesbloqueado);
+                    break;
+                case 2:
+                    lista_media_prioridade.adicionarFim(pDesbloqueado);
+                    break;
+                case 3:
+                    lista_baixa_prioridade.adicionarFim(pDesbloqueado);
+                    break;
+            }
+        }
+
+        if (contador_ciclos_alta_prioridade >= 5) {
+            System.out.println("REGRA ANTI-INANIÇÃO: Executando um processo de prioridade menor.");
+            if (!lista_media_prioridade.estaVazia()) {
+                processoExecutando = lista_media_prioridade.removerInicio();
+            } else if (!lista_baixa_prioridade.estaVazia()) {
+                processoExecutando = lista_baixa_prioridade.removerInicio();
+            }
+            if (processoExecutando != null) {
+                contador_ciclos_alta_prioridade = 0;
+            }
+        }
+
+        if (processoExecutando == null) {
+            if (!lista_alta_prioridade.estaVazia()) {
+                processoExecutando = lista_alta_prioridade.removerInicio();
+                contador_ciclos_alta_prioridade++;
+            } else if (!lista_media_prioridade.estaVazia()) {
+                processoExecutando = lista_media_prioridade.removerInicio();
+            } else if (!lista_baixa_prioridade.estaVazia()) {
+                processoExecutando = lista_baixa_prioridade.removerInicio();
+            }
+        }
+        
 }
